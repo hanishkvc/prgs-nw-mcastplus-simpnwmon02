a758c171e37fa9d9ebde0bde91cb0d38a0eedd79
hanishkvc <hanishkvc@gmail.com>
2018-12-07 07:28:21 +0530

    NwTest data part size can be specified as cmd arg
    The NwTest logic generates packets consisting of
    a) SeqNum and b) Dummy data.
    
    The size of this Dummy data can be specified as a command line arg.
    By default it is 1024 bytes long.

--- hkvc-nw-test.py
+++ hkvc-nw-test.py
@@ -13,6 +13,7 @@ pktid=0
 iArg=1
 port=1111
 N=11
+dataSize=1024
 while iArg < len(sys.argv):
 	if (sys.argv[iArg] == "--port"):
 		iArg += 1
@@ -20,12 +21,15 @@ while iArg < len(sys.argv):
 	elif (sys.argv[iArg] == "--dim"):
 		iArg += 1
 		N = int(sys.argv[iArg])
+	elif (sys.argv[iArg] == "--datasize"):
+		iArg += 1
+		dataSize = int(sys.argv[iArg])
 	iArg += 1
 
-print("port [{}]\ndim [{}]\n".format(port, N))
+print(" port [{}]\n sqmat-dim [{}]\n dataSize [{}]\n".format(port, N, dataSize))
 
 while True:
-	data=struct.pack("<Is", pktid, bytes("Hello world",'utf8'))
+	data=struct.pack("<Is", pktid, bytes(dataSize))
 	sock.sendto(data, ("127.0.0.1", 9999))
 	pktid += 1
 	if ((pktid%(N*N)) == 0):
