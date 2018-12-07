d2c72f1110446840af42f8ae07d5b9b8b7a78c8e
hanishkvc <hanishkvc@gmail.com>
2018-12-07 07:20:37 +0530

    NwTest logic takes port, 2d-squarematrix dimension as args
    If no argument is passed, then
    port = 1111 & dim = 11

--- hkvc-nw-test.py
+++ hkvc-nw-test.py
@@ -10,10 +10,23 @@ import struct
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 pktid=0
+iArg=1
+port=1111
 N=11
+while iArg < len(sys.argv):
+	if (sys.argv[iArg] == "--port"):
+		iArg += 1
+		port=int(sys.argv[iArg])
+	elif (sys.argv[iArg] == "--dim"):
+		iArg += 1
+		N = int(sys.argv[iArg])
+	iArg += 1
+
+print("port [{}]\ndim [{}]\n".format(port, N))
+
 while True:
 	data=struct.pack("<Is", pktid, bytes("Hello world",'utf8'))
-	sock.sendto(data, ("127.0.0.1", 4531))
+	sock.sendto(data, ("127.0.0.1", 9999))
 	pktid += 1
 	if ((pktid%(N*N)) == 0):
 		time.sleep(1)
