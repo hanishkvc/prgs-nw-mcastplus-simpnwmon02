d26816a63d2ed000146fe50d5aef6b4270c0b7ae
hanishkvc <hanishkvc@gmail.com>
2018-12-28 00:01:52 +0530

    UnicastRecovery: Server sends data packets

--- hkvc-nw-recover.py
+++ hkvc-nw-recover.py
@@ -19,7 +19,6 @@ URReqSeqNum = 0xffffff02
 URAckSeqNum = 0xffffff03
 
 
-pktid=0
 port=1111
 N=11
 dataSize=1024
@@ -107,13 +106,29 @@ def presence_info():
 		print(r)
 
 
-def ur_send_packets(lostPackets):
-	print("LostPackets:[{}]".format(lostPackets))
+def gen_lostpackets_array(lostPackets):
+	lp = lostPackets
+	lp = lp.split(b'\n')
+	lpa = []
+	for r in lp:
+		r = r.split(b'-')
+		s = int(r[0])
+		e = int(r[1])
+		for i in range(s,e+1):
+			lpa.append(i)
+	return lpa
+
+
+def ur_send_packets(client, lostPackets):
+	print("ur_send_packets: client[{}] LostPackets:[{}]".format(client, lostPackets))
+	lpa = gen_lostpackets_array(lostPackets)
+	print("ur_send_packets: lostPackets count [{}]".format(len(lpa)))
+	send_file_data(client, lpa)
 
 
 def ur_client(client):
 	global sock
-	print("UnicastRecovery: Listening on [{}:{}] for client [{}]".format(addr, portServer, client))
+	print("UnicastRecovery: Listening on [{}:{}] for client [{}:{}]".format(addr, portServer, client, portClient))
 	startTime = time.time()
 	deltaTime = 0
 	iCnt = 0
@@ -134,7 +149,7 @@ def ur_client(client):
 				print("UR:WARN: Peer has sent wrong resp[{}], skipping the same...".format(hex(cmd)))
 				continue
 			data = dataC[4:]
-			ur_send_packets(data)
+			ur_send_packets(client, data)
 			startTime = time.time()
 		except socket.timeout as e:
 			d = None
@@ -159,40 +174,43 @@ unicast_recovery()
 exit()
 
 
-prevPktid=0
-prevTime=0.0
-curTime=0.0
-prevTimeThrottle=0.0
-while True:
-	if (fData != None):
-		curData = fData.read(dataSize)
-		if (curData == b''):
-			break
-	else:
-		curData = bytes(dataSize)
-	data=struct.pack("<Is", pktid, curData)
-	sock.sendto(data, (addr, port))
-	pktid += 1
-	if ((pktid%N) == 0):
-		curTime = time.time()
-		timeAlloted = (perPktTime*N)
-		timeUsed = curTime-prevTimeThrottle
-		timeRemaining = timeAlloted - timeUsed
-		#print("timeAlloted [{}], timeRemaining[{}]".format(timeAlloted, timeRemaining))
-		if (timeRemaining > 0):
-			(rlist, wlist, xlist) = select.select([0],[],[],timeRemaining)
-			if (len(rlist) == 1):
-				a=input()
-		prevTimeThrottle = time.time()
-	if ((pktid%(N*N*10)) == 0):
-		curTime=time.time()
-		numPkts = pktid - prevPktid
-		timeDelta = curTime - prevTime
-		nwSpeed= ((numPkts*dataSize)/timeDelta)/1e6
-		print("Transfer speed [{}]MBps\n".format(nwSpeed))
-		#time.sleep(1)
-		prevTime = time.time()
-		prevPktid = pktid
+def send_file_data(peer, indexList):
+	prevPktid=0
+	prevTime=time.time()
+	curTime=0.0
+	prevTimeThrottle=time.time()
+	pktid = 0
+	for i in indexList:
+		if (fData != None):
+			fData.seek(i*dataSize)
+			curData = fData.read(dataSize)
+			if (curData == b''):
+				break
+		else:
+			curData = bytes(dataSize)
+		data=struct.pack("<Is", i, curData)
+		sock.sendto(data, (peer, port))
+		pktid += 1
+		if ((pktid%N) == 0):
+			curTime = time.time()
+			timeAlloted = (perPktTime*N)
+			timeUsed = curTime-prevTimeThrottle
+			timeRemaining = timeAlloted - timeUsed
+			#print("timeAlloted [{}], timeRemaining[{}]".format(timeAlloted, timeRemaining))
+			if (timeRemaining > 0):
+				(rlist, wlist, xlist) = select.select([0],[],[],timeRemaining)
+				if (len(rlist) == 1):
+					a=input()
+			prevTimeThrottle = time.time()
+		if ((pktid%(N*N*10)) == 0):
+			curTime=time.time()
+			numPkts = pktid - prevPktid
+			timeDelta = curTime - prevTime
+			nwSpeed= ((numPkts*dataSize)/timeDelta)/1e6
+			print("Transfer speed [{}]MBps\n".format(nwSpeed))
+			#time.sleep(1)
+			prevTime = time.time()
+			prevPktid = pktid
 
 
 print("INFO: Done with transfer")
