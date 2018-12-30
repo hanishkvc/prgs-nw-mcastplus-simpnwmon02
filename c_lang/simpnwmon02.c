c577157293678badd353a57d4bb95763c7d3ca0a
hanishkvc <hanishkvc@gmail.com>
2018-12-30 22:39:43 +0530

    Basic skeleton for Pkt Recv & Data Save
    Added logic which recieves mcast packets, checks for consistency in seqNum in
    the pkts, and notes any issues suitabley. Inturn the recvd data from the pkt,
    is saved to the specified data file.
    
    Currently the fileData related logic is not yet fleshed out, beyond a basic
    skeleton.

--- c_lang/simpnwmon02.c
+++ c_lang/simpnwmon02.c
@@ -4,7 +4,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
 
+const int MCAST_USLEEP=500;
+const int PKT_SEQNUM_OFFSET=0;
+const int PKT_DATA_OFFSET=4;
 
 int portMCast=1111;
 int portServer=1112;
@@ -12,6 +18,12 @@ int portClient=1113;
 
 char *sLocalAddr="0.0.0.0";
 char *sPIInitAddr="255.255.255.255";
+
+#define BUF_MAXSIZE 1600
+int giDataSize=(1024+4);
+char gcBuf[BUF_MAXSIZE];
+
+int gbDoMCast = 1;
 
 int sock_mcast_init_ex(int ifIndex, char *sMCastAddr)
 {
@@ -41,6 +53,53 @@ int sock_mcast_init_ex(int ifIndex, char *sMCastAddr)
 	return sockMCast;
 }
 
+
+int filedata_save(int fileData, char *buf, int iBlockOffset, int len) {
+	if (len != giDataSize) {
+		fprintf(stderr, "WARN:%s:giDataSize[%d] != pktDataLen[%d]\n", __func__, giDataSize, len);
+	}
+	lseek(fileData, iBlockOffset*giDataSize, SEEK_SET);
+	write(fileData, buf, len);
+}
+
+int mcast_recv(int sockMCast, int fileData) {
+
+	int iRet;
+	uint32_t iPrevSeq = 0;
+	uint32_t iSeq = 0;
+	int iPktCnt = 0;
+	int iOldSeqs = 0;
+	int iDisjointSeqs = 0;
+	int iDisjointPktCnt = 0;
+	int iSeqDelta;
+
+	while (gbDoMCast) {
+
+		iRet = recvfrom(sockMCast, gcBuf, giDataSize, MSG_DONTWAIT, NULL, NULL);
+		if (iRet == -1) {
+			if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+				usleep(MCAST_USLEEP);
+			} else {
+				perror("WARN:mcast_recv:revfrom failed:");
+			}
+			continue;
+		}
+		iPktCnt += 1;
+		iSeq = *((uint32_t*)&gcBuf[PKT_SEQNUM_OFFSET]);
+		iSeqDelta = iSeq - iPrevSeq;
+		if (iSeqDelta <= 0) {
+			iOldSeqs += 1;
+			continue;
+		}
+		if (iSeqDelta > 1) {
+			iDisjointSeqs += 1;
+			iDisjointPktCnt += (iSeqDelta-1);
+		}
+		filedata_save(fileData, &gcBuf[PKT_DATA_OFFSET], iSeq, iRet-PKT_DATA_OFFSET);
+
+	}
+
+}
 
 int main(int argc, char **argv) {
 
