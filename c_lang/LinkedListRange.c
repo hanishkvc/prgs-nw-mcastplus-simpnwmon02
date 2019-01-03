b9f800b927671c79d4e035bbe6db132cacc48632
hanishkvc <hanishkvc@gmail.com>
2019-01-03 15:28:33 +0530

    LinkedListRange - Delete, _alloc
    Moved the logic which allocates a node and initialises it's members with
    suitable values from a standalone perspective, into a new _ll_alloc function.
    
    Inturn this is used by the existing ll_add_sorted and the new ll_delete logics.
    
    Have added a delete logic, which takes a single value and then updates the list
    of ranges such that this value is excluded from that list. This could involve
    removing node if it represented this single value or else adjusting existing
    ranges as required. If the val is the begin or end of a existing range, just
    that begin/start or end needs to be suitably updated. However the given value
    falls within a given range, then that range requires to be split into two new
    ranges, such that this particular value is excluded.

--- c_lang/LinkedListRange.c
+++ c_lang/LinkedListRange.c
@@ -15,17 +15,26 @@ void ll_init(struct LLR *me) {
 	me->llStart = NULL;
 }
 
+struct _ll *_ll_alloc(int start, int end)
+{
+	struct _ll *llTemp;
+	llTemp = malloc(sizeof(struct _ll));
+	if (llTemp == NULL) {
+		fprintf(stderr, "ERROR:%s: NoMemory to allocate new node for [%d-%d]\n", __func__, start, end);
+		return NULL;
+	}
+	llTemp->rStart = start;
+	llTemp->rEnd = end;
+	llTemp->prev = NULL;
+	llTemp->next = NULL;
+	return llTemp;
+}
+
 int ll_add_sorted(struct LLR *me, int start, int end) {
 	struct _ll *llTemp;
 	struct _ll *llNext, *llPrev;
 
-	llTemp = malloc(sizeof(struct _ll));
-	if (llTemp == NULL) {
-		fprintf(stderr, "ERROR:%s: NoMemory to allocate new node for [%d-%d]\n", __func__, start, end);
-		return -1;
-	}
-	llTemp->rStart = start;
-	llTemp->rEnd = end;
+	llTemp = _ll_alloc(start, end);
 	llPrev = NULL;
 	llNext = me->llStart;
 	while (llNext != NULL) {
@@ -49,6 +58,66 @@ int ll_add_sorted(struct LLR *me, int start, int end) {
 	}
 	fprintf(stderr, "INFO:%s: Added [%d-%d]\n", __func__, start, end);
 	return 0;
+}
+
+void _ll_delete(struct LLR *me, struct _ll *llDel) {
+	struct _ll *llNext, *llPrev;
+	if (me->llStart == llDel) {
+		me->llStart = llDel->next;
+		if (me->llStart != NULL) {
+			me->llStart->prev = NULL;
+		}
+		return;
+	}
+	llPrev = llDel->prev;
+	llNext = llDel->next;
+	llPrev->next = llNext;
+	if (llNext != NULL) {
+		llNext->prev = llPrev;
+	}
+}
+
+int ll_delete(struct LLR *me, int val) {
+	struct _ll *llTemp;
+	struct _ll *llNext, *llPrev;
+
+	llPrev = NULL;
+	llNext = me->llStart;
+	while(llNext != NULL) {
+		if (llNext->rStart == val) {
+			if (llNext->rEnd == val) {
+				_ll_delete(me, llNext);
+				return 0;
+			} else {
+				llNext->rStart += 1;
+				return 0;
+			}
+		}
+		if (llNext->rEnd == val) {
+			if (llNext->rStart == val) {
+				// shouldn't reach here, it should have matched the (start==val => end==val) test above
+				fprintf(stderr, "DEBUG:%s: Shouldn't reach here for removing [%d]\n", __func__, val);
+				exit(-1);
+			} else {
+				llNext->rEnd -= 1;
+				return 0;
+			}
+		}
+		if ((llNext->rStart < val) && (llNext->rEnd > val)) {
+			int iEnd = llNext->rEnd;
+			llNext->rEnd = val-1;
+			llTemp = _ll_alloc(val+1, iEnd);
+			llTemp->prev = llNext;
+			llTemp->next = llNext->next;
+			llNext->next = llTemp;
+			if (llTemp->next != NULL) {
+				llTemp->next->prev = llTemp;
+			}
+			return 0;
+		}
+		llNext = llNext->next;
+	}
+	return -1;
 }
 
 int ll_free(struct LLR *me) {
