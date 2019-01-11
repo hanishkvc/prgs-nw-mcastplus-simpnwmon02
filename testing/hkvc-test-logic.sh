3310d9dddf478f31b562953c2caf93a3e9e0d019
hanishkvc <hanishkvc@gmail.com>
2019-01-11 13:32:10 +0530

    Testing: Updated message printed wrt TotalBlocks to check
    Also added a new command to generate a 10GB file with holes except for the last
    1024 block. This is similar to the 5GB file which I created previously.
    
    With these holes in the file, one can get the effect of a large file, without
    actually requiring that much space to be available on the filesystem. i.e in
    filesystem only 1024 bytes will be used these 5GB and 10GB files.
    
    NOTE: However the simpnwmon02 will generate a actual 5GB or 10GB file, so
    remember to have space in the filesystem into which simpnwmon02 may write such
    files.

--- testing/hkvc-test-logic.sh
+++ testing/hkvc-test-logic.sh
@@ -4,17 +4,24 @@
 dd if=/dev/zero of=/tmp/test.9999.10 bs=10 count=1
 dd if=/dev/zero of=/tmp/test.9999.1034 bs=1034 count=1
 ll /tmp/
-echo "TotalBlocks to Transfer should be printed as 1"
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 1"
 ../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.10
-echo "TotalBlocks to Transfer should be printed as 2"
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 2"
 ../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.1034
 
 dd if=/dev/zero of=/tmp/test.9999.1024 bs=1024 count=1
 
 dd if=/dev/zero of=/tmp/test.9999.5G bs=1024 seek=5242879 count=1
+dd if=/dev/zero of=/tmp/test.9999.10G bs=1024 seek=10485759 count=1
+
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 2"
 ../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.1034
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 2"
 ../server/hkvc-nw-recover.py --file /tmp/test.9999.1034
+
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 5242880"
 ../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.5G
+echo "**** **** **** **** TotalBlocks to Transfer should be printed as 5242880"
 ../server/hkvc-nw-recover.py --file /tmp/test.9999.5G
 
 ../server/hkvc-nw-recover.py --file /tmp/test.9999.5G --context ./t.context
