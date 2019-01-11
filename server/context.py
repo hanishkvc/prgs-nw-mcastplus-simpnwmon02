828b4dbef6bbeb0c181680d7fd4804f951d883f9
hanishkvc <hanishkvc@gmail.com>
2019-01-11 10:44:58 +0530

    Srvr:Context: module to manage offline context
    Added a new module, which helps manage a offline context.
    
    Currently it allows one to load a list of clients
    
    NOTE: Tried calling __builtins__.open, but didn't work. Maybe I have to
    explicitly import __builtins__, for now I am importing io

---
+++ server/context.py
@@ -0,0 +1,43 @@
+#
+# context - a logic to load program related context from a given text file
+# v20190111IST1031
+# HanishKVC
+#
+import io
+import enum
+
+def open(sfContext, sMode="r"):
+	fContext = io.open(sfContext, sMode)
+	return fContext
+
+
+LCMode=enum.Enum("lcMode", "NONE, CLIENTS")
+def load_clients(fContext):
+	fContext.seek(0)
+	lcMode = LCMode.NONE
+	clients = []
+	while True:
+		l = fContext.readline()
+		if (l == ""):
+			break
+		l = l.strip()
+		if (l.startswith("<clients>")):
+			if (lcMode != LCMode.NONE):
+				print("ERROR:load_clients: logic state wrong at clients_begin")
+				break
+			lcMode = LCMode.CLIENTS
+		elif (l.startswith("</clients>")):
+			if (lcMode != LCMode.CLIENTS):
+				print("ERROR:load_clients: logic state wrong at clients_end")
+				break
+			lcMode = LCMode.NONE
+			break
+		else:
+			if (lcMode == LCMode.CLIENTS):
+				clients.append(l)
+	return clients
+
+
+def close(fContext):
+	fContext.close()
+
