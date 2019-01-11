398310c1bdef7a698bab6660e62e247acef8d54a
hanishkvc <hanishkvc@gmail.com>
2019-01-11 11:58:53 +0530

    Testing: More test commands added, if required

--- testing/hkvc-test-logic.sh
+++ testing/hkvc-test-logic.sh
@@ -9,3 +9,12 @@ echo "TotalBlocks to Transfer should be printed as 1"
 echo "TotalBlocks to Transfer should be printed as 2"
 ../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.1034
 
+dd if=/dev/zero of=/tmp/test.9999.1024 bs=1024 count=1
+
+dd if=/dev/zero of=/tmp/test.9999.5G bs=1024 seek=5242879 count=1
+../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.1034
+../server/hkvc-nw-recover.py --file /tmp/test.9999.1034
+../server/hkvc-nw-send-mcast.py --file /tmp/test.9999.5G
+../server/hkvc-nw-recover.py --file /tmp/test.9999.5G
+
+../server/hkvc-nw-recover.py --file /tmp/test.9999.5G --context ./t.context
