b6dbfabccdbe5b05ba2c2258c7e77614a228da4d
hanishkvc <hanishkvc@gmail.com>
2019-01-11 23:28:56 +0530

    Notes and TODO updated wrt Aggressive PI logic
    Also added a new TODO wrt making the UCast Recovery faster by reducing the time
    given to inactive/non-responding clients.
    
    Updated qemu-tftp-get-files.input to also get the new network module added.
    
    Also added a shell script that helps send all required programs to /tmp/share
    folder so that same can be picked from the qemu vm using its builtin tftp
    server logic, which is available in case the host side user space network
    support logic of qemu is used for supporting networking in the VMs.

---
+++ hkvc-install-for-vm.sh
@@ -0,0 +1,5 @@
+cp -a server/*.py /tmp/share/
+cp -a client/c_lang/simpnwmon02 /tmp/share
+cp -a testing/hkvc-check-image.py /tmp/share
+cp -a testing/t.context /tmp/share
+cp -a testing/qemu-tftp-get-files.input /tmp/share
