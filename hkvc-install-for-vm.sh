9247e22d066da4de8d27737094ebd891438fdc98
hanishkvc <hanishkvc@gmail.com>
2019-01-12 11:48:24 +0530

    HelperScript for vm based testing updated

--- hkvc-install-for-vm.sh
+++ hkvc-install-for-vm.sh
@@ -1,5 +1,9 @@
-cp -a server/*.py /tmp/share/
-cp -a client/c_lang/simpnwmon02 /tmp/share
-cp -a testing/hkvc-check-image.py /tmp/share
-cp -a testing/t.context /tmp/share
-cp -a testing/qemu-tftp-get-files.input /tmp/share
+
+VM_SHARE_DIR=/tmp/share
+mkdir $VM_SHARE_DIR
+cp -a server/*.py $VM_SHARE_DIR/
+cp -a client/c_lang/simpnwmon02 $VM_SHARE_DIR/
+cp -a testing/hkvc-check-image.py $VM_SHARE_DIR/
+cp -a testing/t.context $VM_SHARE_DIR/
+cp -a testing/qemu-tftp-get-files.input $VM_SHARE_DIR/
+
