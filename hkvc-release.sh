39cffded19d268ebd15f7e57c6099239342aceb0
hanishkvc <hanishkvc@gmail.com>
2019-03-30 22:35:59 +0530

    HelperScript release updated and made flexible
    for now supports only
    
    a) No args or args --bin --srv
    
    OR
    
    b) --all

--- hkvc-release.sh
+++ hkvc-release.sh
@@ -1,12 +1,52 @@
 
+
+typeSB="bin"
+typeSCA="srv"
+
+for i in $@; do
+  echo $i
+  if [ "$i" == "src" ]; then
+    typeSB="src"
+  fi
+  if [ "$i" == "bin" ]; then
+    typeSB="bin"
+  fi
+  if [ "$i" == "srv" ]; then
+    typeSCA="srv"
+  fi
+  if [ "$i" == "cli" ]; then
+    typeSCA="cli"
+  fi
+  if [ "$i" == "all" ]; then
+    typeSCA="all"
+  fi
+done
+
+echo "typeSB [$typeSB]"
+echo "typeSCA [$typeSCA]"
+
+
 base="../tmp/release"
 date=`date +%Y%m%d%Z%H%M`
-dir="SimpNwMon02_v$date"
+dir="SimpNwMon02_v$date-$typeSB-$typeSCA"
 fdir=$base/$dir
 
+echo "dir [$dir]"
+
 mkdir -p $fdir
-cp -a . $fdir/
-cd $base/
+if [ "$typeSCA" == "srv" ]; then
+  if [ "$typeSB" == "bin" ]; then
+    mkdir $fdir/srv
+    cp -a server/python/*so $fdir/srv/
+    cp -a server/python/hkvc-nw-send-mcast.py $fdir/srv/
+  fi
+elif [ "$typeSCA" == "all" ]; then
+  cp -a . $fdir/
+fi
+
+echo "base [$base], type exit to continue"
+pushd $base/
+bash
 7z -p a $dir.7z $dir
 pwd
 ls -al
