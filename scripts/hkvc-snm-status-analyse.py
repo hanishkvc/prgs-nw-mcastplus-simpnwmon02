e5b2a6742c4bdefea79395f1065ba66afac91d9e
hanishkvc <hanishkvc@gmail.com>
2019-04-01 20:58:33 +0530

    Script: New script to analyse snm.srv.status.log
    The initial implementation of a python script, which helps parse
    the snm02.srv.status.log and identify
    
    a) if for a given IP address, the device name has changed or not.
    
    b) count how many times a given ip/device has successfully responded
    to the PI query.

---
+++ scripts/hkvc-snm-status-analyse.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# snm-status-analyse : Verifies how many times a given ip/device has responded to PI request from server
+# HanishKVC
+#
+
+import sys
+
+sInFile = sys.argv[1]
+fInFile = open(sFile)
+
+def parse_file(fFile):
+	dC = {}
+	for l in fFile:
+		l = l.strip()
+		if (not l.startswith("UCAST_PI")):
+			continue
+		la = l.split(':')
+		sIP = la[1].split('=')[1]
+		iLP = int(la[2].split('=')[1])
+		iCnt = int(la[3].split('=')[1])
+		sName = la[4].split('=')[1]
+		if (iCnt == 0):
+			continue
+		try:
+			sNameInD = dC[sIP][0]
+			if (sNameInD != sName):
+				print("WARN:NameChange: for IP[{}] from [{}] to [{}]".format(sIP, sNameInD, sName))
+			dC[sIP] = [ sName, dC[sIP][1]+1 ]
+		except:
+			dC[sIP] = [ sName, 0 ]
+	return dC
+
+dC = parse_file(fInFile)
+print(dC)
+
+def dict2sortlist(dIn):
+	lstNew = []
+	for k in dIn:
+		bDone = False
+		for i in range(len(lstNew)):
+			if (lstNew[1][1] < dIn[k][1]):
+				continue
+			lstNew.insert(i, [ k, dIn[k] ])
+			bDone = True
+		if (not bDone):
+			lstNew.append(k, dIn[k])
+	return lstNew
+
+lC = dict2sortlist(dC)
+print(lC)
+
