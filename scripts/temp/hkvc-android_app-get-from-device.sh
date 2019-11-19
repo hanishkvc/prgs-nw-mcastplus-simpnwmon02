afe8119b5547c241c80bba81322eceb6d41682ff
hanishkvc <hanishkvc@gmail.com>
2019-11-19 15:04:09 +0530

    Commiting prev logs and android app helper script
    Logs of testing transfer in a enclosed tightly packed with metal objects
    in-between based end setup with diff wifi settings.
    
    NOTE: This setup is bad for wifi mcast at one level. But still check how
    things behave.
    
    NOTE: Its been found that letting the AP do the mcast to ucast based conversion
    to achieve a high speed transfer where possible, is not upto the mark. Rather
    handling parallel ucasts manually potentially gives much much better results.
    
    A helper script to pull the data and logs from the android app on
    a device.

---
+++ scripts/temp/hkvc-android_app-get-from-device.sh
@@ -0,0 +1,3 @@
+/home/hanishkvc/Android/Sdk/platform-tools/adb pull /storage/sdcard/Android/data/india.hanishkvc.simpnwmon02/files/data.bin
+/home/hanishkvc/Android/Sdk/platform-tools/adb pull /storage/sdcard/Android/data/india.hanishkvc.simpnwmon02/files/lost.log
+/home/hanishkvc/Android/Sdk/platform-tools/adb pull /storage/sdcard/Android/data/india.hanishkvc.simpnwmon02/files/lost.log.txt
